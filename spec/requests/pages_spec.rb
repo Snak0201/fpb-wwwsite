e5577e7710d6bca5287d00fce4664f39cc26d134
Snak <72hoshino.snak@gmail.com>
2024-01-04 17:49:09 +0900

    fix rubocop

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Pages", type: :request do
-  describe "GET /top" do
-    it "returns http success" do
-      get "/pages/top"
+RSpec.describe 'Pages' do
+  describe 'GET /' do
+    it 'returns http success' do
+      get '/'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
