c757405f63bf83c9aac608323d86e0a74b64fe74
Snak <72hoshino.snak@gmail.com>
2024-01-13 10:31:44 +0900

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_12_021009) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_12_151719) do
+  create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.text "content"
@@ -21,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_12_021009) do
     t.index ["number"], name: "index_articles_on_number"
     t.index ["title"], name: "index_articles_on_title"
   end
+
 end
