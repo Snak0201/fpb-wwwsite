818d7d1592fbbe00f4f04acc54c672dadacd977d
Snak <snak.hoshinonaka@gmail.com>
2024-01-16 09:31:05 +0900

    add articles_spec

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,12 +1,12 @@
 Capybara.register_driver :chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
+  options = Selenium::WebDriver::Chrome::Options.new
 
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--window-size=1400,1400')
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
 
 Capybara.javascript_driver = :chrome_headless
@@ -16,11 +16,11 @@ RSpec.configure do |config|
     driven_by :rack_test
   end
 
-  config.before(:each, type: :system, js: true) do
-    if ENV["SELENIUM_DRIVER_URL"].present?
+  config.before(:each, :js, type: :system) do
+    if ENV['SELENIUM_DRIVER_URL'].present?
       driven_by :selenium, using: :chrome, options: {
         browser: :remote,
-        url: ENV.fetch("SELENIUM_DRIVER_URL"),
+        url: ENV.fetch('SELENIUM_DRIVER_URL'),
         desired_capabilities: :chrome
       }
     else
--- spec/system/root_spec.rb
+++ spec/system/root_spec.rb
@@ -2,6 +2,6 @@ RSpec.describe 'root_path' do
   it do
     visit root_path
     click_on 'Hoshinonaka Government'
-    expect(page).to have_content 'トップ'
+    expect(page).to have_content 'ほしのなか政府について'
   end
 end
