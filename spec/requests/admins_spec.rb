7e3194b22e48c2b0e59a40236393348ecf83eb80
Snak <72hoshino.snak@gmail.com>
2024-01-24 15:01:08 +0900

    管理ページのrequest specを追加

--- spec/requests/admins_spec.rb
+++ spec/requests/admins_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin' do
+  let(:admin) { create(:admin) }
+
   describe 'GET /' do
-    it 'returns http found' do
-      get '/admin/'
-      expect(response).to have_http_status(:found)
+    context 'without login' do
+      it 'returns http found' do
+        get '/admin/'
+        expect(response).to have_http_status(:found)
+      end
+    end
+
+    context 'with login' do
+      it 'returns http success' do
+        sign_in admin
+        get '/admin/'
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
