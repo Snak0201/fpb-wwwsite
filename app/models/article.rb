fe45b43dd8e83dfcfd5369a80da1b3fa9b8bc68e
Snak <72hoshino.snak@gmail.com>
2024-01-27 13:11:08 +0900

    コメントを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
-  has_many :bureau_articles, dependent: :destroy
+  # foreign_keyに中間テーブルにあるカラム名を指定する
+  # class_nameはなくても良いが、書くと親切
+  has_many :bureau_articles, dependent: :destroy, class_name: 'BureauArticle', foreign_key: :government_bureau_id
+  # a.government_bureausの形でbureauを取得できる
+  # throughを忘れないようにする
   has_many :government_bureaus, through: :bureau_articles
 end
--- app/models/bureau_article.rb
+++ app/models/bureau_article.rb
@@ -1,7 +1,8 @@
 class BureauArticle < ApplicationRecord
   # associations名を変更する
   # associations名からクラス名を推測できないので指定する
-  belongs_to :government_bureau, class_name: 'Bureau'
+  # foreign_keyに参照先のモデルのカラム名を指定する
+  belongs_to :government_bureau, class_name: 'Bureau', foreign_key: :id
   # belongs_to :bureau
-  belongs_to :article
+  belongs_to :article, foreign_key: :id
 end
