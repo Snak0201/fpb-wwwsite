10ed588691d416f845c2b0949f76b7938ddb316b
Snak <72hoshino.snak@gmail.com>
2024-01-27 10:33:25 +0900

    これだとうまくいかない `An error occurred when running Kernel#inspect: #<ActiveRecord::StatementInvalid: Mysql2::Error: Unknown column 'bureaus.article_id' in 'where clause'>`

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  has_many :bureau_articles
+  has_many :bureaus, through: :bureau_articles
 end
--- app/models/bureau.rb
+++ app/models/bureau.rb
@@ -1,2 +1,4 @@
 class Bureau < ApplicationRecord
+  has_many :bureau_articles
+  has_many :articles, through: :bureau_articles
 end
