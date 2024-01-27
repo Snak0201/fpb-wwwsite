e28f0e9fb6d77e3a7710f01affaeb20de5a62656
Snak <72hoshino.snak@gmail.com>
2024-01-27 17:26:57 +0900

    モデルに制約を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  has_many :bureau_articles, class_name: 'BureauArticle', foreign_key: :bureau_id, inverse_of: :article,
+                             dependent: :destroy
+  has_many :bureaus, through: :bureau_articles
 end
