21e5cb4eaf1c7af6ce67c2f91562e19d1b5bc614
Snak <72hoshino.snak@gmail.com>
2024-01-31 18:41:21 +0900

    フォームの共通化を整理

--- app/controllers/admins/articles_controller.rb
+++ app/controllers/admins/articles_controller.rb
@@ -14,10 +14,12 @@ module Admins
       @article = Article.new
     end
 
-    def edit; end
+    def edit
+      @article = Article.find(params[:id])
+    end
 
     def create
-      @article = Article.new(create_article_params)
+      @article = Article.new(article_params)
 
       if @article.save
         redirect_to admin_articles_path, notice: '記事が登録されました'
@@ -26,11 +28,19 @@ module Admins
       end
     end
 
-    def update; end
+    def update
+      @article = Article.find(params[:id])
+
+      if @article.update(article_params)
+        redirect_to admin_articles_path, notice: '記事が更新されました'
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
 
     private
 
-    def create_article_params
+    def article_params
       params.require(:article).permit(%i[title content number])
     end
   end
