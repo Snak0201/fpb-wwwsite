f265d35b8c4cd7bc340e7fa2597c469fd72691d6
Snak <72hoshino.snak@gmail.com>
2024-01-31 13:58:25 +0900

    記事登録フォームを追加

--- app/controllers/admins/articles_controller.rb
+++ app/controllers/admins/articles_controller.rb
@@ -1,17 +1,29 @@
 class Admins::ArticlesController < ApplicationController
+  def index
+    @articles = Article.order(:id)
+  end
+
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
-    @articles = Article.new
+    @article = Article.new
   end
 
   def edit
   end
 
   def create
+    article = Article.new(create_article_params)
+    puts article.valid?
   end
 
   def update
+  end
+
+  private
+  def create_article_params
+    params.require(:article).permit(%i[title content number])
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   has_many :bureau_articles, class_name: 'BureauArticle', foreign_key: :bureau_id, inverse_of: :article,
                              dependent: :destroy
   has_many :bureaus, through: :bureau_articles
+
+  validates :title, :number, presence: true
 end
