a8f2602407b399153fb045a20ec6723df422728b
Snak <72hoshino.snak@gmail.com>
2024-02-04 11:33:45 +0900

    Rubocopの例外を除去

--- spec/requests/pages_controller_spec.rb
+++ spec/requests/pages_controller_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe PagesController do
   describe 'GET /' do
     context 'with 6 articles' do
-      let!(:articles) { create_list(:article, 6, :published) }
       let(:newest_articles) { Article.where.not(published_at: nil).order(published_at: :desc).limit(5) }
+
+      before do
+        create_list(:article, 6, :published)
+      end
 
       it 'shows the newest 5 articles' do
         get '/'
@@ -16,10 +19,13 @@ RSpec.describe PagesController do
     end
 
     context 'with 6 published articles and newer 5 unpublished articles' do
-      let!(:published_articles) { create_list(:article, 6, :published) }
-      let!(:unpublished_articles) { create_list(:article, 5) }
       let(:newest_articles) { Article.where.not(published_at: nil).order(published_at: :desc).limit(5) }
       let(:invisible_articles) { Article.where(published_at: nil) }
+
+      before do
+        create_list(:article, 6, :published)
+        create_list(:article, 5)
+      end
 
       it 'shows the newest 5 published articles' do
         get '/'
