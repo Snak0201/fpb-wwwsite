8b6627c99fb20efada225984325b1ad424af541e
Snak <72hoshino.snak@gmail.com>
2024-02-07 21:18:08 +0900

    文字数カウンターにタイトルを設置

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def page_title(title='')
-    title.present? ? "#{title} | ほしのなか政府": "ほしのなか政府"
+  def page_title(title = '')
+    title.present? ? "#{title} | ほしのなか政府" : 'ほしのなか政府'
   end
 end
