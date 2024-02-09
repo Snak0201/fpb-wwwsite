42f9345117cfab553c0fc8a771b7fdda88cebfc7
Snak <snak.hoshinonaka@gmail.com>
2024-02-10 01:42:14 +0900

    お知らせ更新の追加

--- app/controllers/admins/announcements_controller.rb
+++ app/controllers/admins/announcements_controller.rb
@@ -1,15 +1,36 @@
 module Admins
   class AnnouncementsController < DeviseAuthenticationController
     def edit
-      @announcement = Announcement.last
+      # NOTE: キャンセルボタンが押された場合
+      @announcement = if params[:admins_announcement]
+                        Announcement.new(announcement_params)
+                      else
+                        Announcement.last
+                      end
     end
 
-    def update; end
+    def update
+      @announcement = Announcement.last
+
+      if params[:commit] == '更新'
+        if @announcement.update(announcement_params)
+          redirect_to admin_index_path, notice: 'お知らせが更新されました。'
+        else
+          @announcement = Announcement.new(announcement_params)
+          render :edit, status: :unprocessable_entity
+        end
+      elsif params[:commit] == 'キャンセル'
+        @announcement = Announcement.new(announcement_params)
+        render :edit, status: :see_other
+      end
+    end
 
     def preview
       @announcement = Announcement.new(announcement_params)
       render :preview, status: :see_other
     end
+
+    private
 
     def announcement_params
       params.require(:admins_announcement).permit(:content)
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def top
     @articles = Article.newest
+    @announcement = Admins::Announcement.last
   end
 
   def about; end
