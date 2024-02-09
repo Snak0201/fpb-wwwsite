1ca297dcac6614ca0c402fed8212dd5670064ec8
Snak <snak.hoshinonaka@gmail.com>
2024-02-09 19:43:36 +0900

    controller specの修正

---
+++ spec/requests/admins/announcements_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Admins::AnnouncementsController do
+  context 'when not log in' do
+    describe 'GET /edit' do
+      it 'returns http found' do
+        get '/admin/announcement/edit'
+        expect(response).to have_http_status(:found)
+      end
+    end
+  
+    describe 'GET /preview' do
+      it 'returns http found' do
+        get '/admin/announcement/preview'
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
+
+  context 'when log in' do
+    let(:admin) { create(:admin) }
+
+    before do
+      sign_in admin
+    end
+
+    describe 'GET /edit' do
+      it 'returns http success' do
+        get '/admin/announcement/edit'
+        expect(response).to have_http_status(:success)
+      end
+    end
+  
+    describe 'GET /preview' do
+      it 'returns http success' do
+        get '/admin/announcement/preview'
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+end
