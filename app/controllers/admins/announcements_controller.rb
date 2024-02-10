eaf92ab79cee1455e82851e1ee71bfba0b2e6280
Snak <72hoshino.snak@gmail.com>
2024-02-10 23:34:41 +0900

    プレビュー画面の確認

--- app/controllers/admins/announcements_controller.rb
+++ app/controllers/admins/announcements_controller.rb
@@ -6,6 +6,16 @@ module Admins
 
     def create; end
 
-    def preview; end
+    def preview
+      @announcement = Announcement.new(announcement_params)
+      render :preview, status: :see_other
+    end
+
+    private
+
+    def announcement_params
+      params.require(:admins_announcement).permit(:content)
+    end
+
   end
 end
