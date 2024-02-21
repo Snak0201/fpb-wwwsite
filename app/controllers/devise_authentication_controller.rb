e4f48192a41610e50afdcb40b2dc4ea35063e9b1
Snak <72hoshino.snak@gmail.com>
2024-02-21 13:12:21 +0900

    punditを使えるように設定

--- app/controllers/devise_authentication_controller.rb
+++ app/controllers/devise_authentication_controller.rb
@@ -1,3 +1,8 @@
 class DeviseAuthenticationController < ApplicationController
+  include Pundit::Authorization
   before_action :authenticate_admin!
+
+  def pundit_user
+    current_admin
+  end
 end
