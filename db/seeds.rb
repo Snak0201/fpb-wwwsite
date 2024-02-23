f4983d57196fa0d85b83010c464ca243b2be2ffb
Snak <72hoshino.snak@gmail.com>
2024-02-23 14:34:36 +0900

    Seed操作の除去

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,6 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # seed操作
-BUREAUS = [
-  { name: '総務局', slug: 'general', description: '生活事項全般の雑多な記事' },
-  { name: '財務局', slug: 'finance', description: 'ほしのなか政府の財政・お金関係の記事' },
-  { name: '法務局', slug: 'justice', description: 'ほしのなか政府で策定、議論された規則や条例・その他法律に関する記事' },
-  { name: '医務局', slug: 'medical', description: '健康・医薬品などの記事' },
-  { name: '労務局', slug: 'labor', description: '仕事・就職に関する記事' },
-  { name: '学務局', slug: 'education', description: '教育・資格に関する記事' },
-  { name: '外交局', slug: 'foreign', description: 'イベント・交流などの記事' },
-  { name: '娯楽局', slug: 'entertainment', description: 'ゲーム・カラオケなどの趣味、その考え方に関する記事' },
-  { name: '広報局', slug: 'publish', description: 'ほしのなか政府の宣伝・Web構成上のテック記事など' }
-].freeze
-
-Bureau.all.zip(BUREAUS).each do |bureau, info|
-  bureau.update(info)
-end
 
 # IDが0の記事を初期値に戻す
 article = Article.find_or_create_by(id: 0)
