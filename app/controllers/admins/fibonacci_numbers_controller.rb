b83b0158ee570a7fb3d7c9a5a54b7058df26db39
Snak <72hoshino.snak@gmail.com>
2024-03-16 21:13:51 +0900

    ファイルの追加

---
+++ app/controllers/admins/fibonacci_numbers_controller.rb
@@ -0,0 +1,7 @@
+module Admins
+  class FibonacciNumbersController < ApplicationController
+    def show; end
+
+    def create; end
+  end
+end
---
+++ app/helpers/admins/fibonacci_numbers_helper.rb
@@ -0,0 +1,2 @@
+# module Admins::FibonacciNumbersHelper
+# end
