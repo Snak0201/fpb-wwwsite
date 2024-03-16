cd35941ffddfa44135535d977dab32a95c54df1f
Snak <72hoshino.snak@gmail.com>
2024-03-16 22:56:04 +0900

    FaradayでAPIを叩く処理を追加

--- app/controllers/admins/fibonacci_numbers_controller.rb
+++ app/controllers/admins/fibonacci_numbers_controller.rb
@@ -1,7 +1,16 @@
 module Admins
   class FibonacciNumbersController < ApplicationController
-    def show; end
+    def show
+      connection = Faraday.new 'https://fpbwwwapi.pythonanywhere.com' do |conn|
+        conn.response :json
+      end
 
-    def create; end
+      response = connection.get '/api/fibonacci/v1/number', n: 7
+      @number = response.body['value']
+    end
+
+    def create
+
+    end
   end
 end
