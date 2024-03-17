f79c89ba0f893182a7e461859f68e78901b418bd
Snak <72hoshino.snak@gmail.com>
2024-03-17 20:12:07 +0900

    specファイルを改名

--- spec/requests/admins/fibonacci_numbers_controller_spec.rb
+++ spec/requests/admins/fibonacci_numbers_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Admins::FibonacciNumbers' do
+RSpec.describe Admins::FibonacciNumbersController do
   describe 'GET /show' do
     it 'returns http success' do
       get admin_fibonacci_number_path
