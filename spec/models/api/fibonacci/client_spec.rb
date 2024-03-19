c751918f99e256c4b1bd8efe09c55746318f7a74
Snak <72hoshino.snak@gmail.com>
2024-03-19 15:42:16 +0900

    テストにモックを追加

--- spec/models/api/fibonacci/client_spec.rb
+++ spec/models/api/fibonacci/client_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Api::Fibonacci::Client do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:mocked_client){ double(described_class) }
+
+  before do
+    allow(described_class).to recieve(:new).and_return(mocked_client)
+    allow(mocked_client).to recieve(:get_fibonacci_number).with(10).and_return(55)
+  end
 end
