bd732d1e18e8cd4122b8580c6ea06371cc748e9d
Snak <72hoshino.snak@gmail.com>
2024-03-28 16:38:24 +0900

    カラム名をログでフィルタ出来るように修正

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,6 +3,8 @@
 # Configure parameters to be filtered from the log file. Use this to limit dissemination of
 # sensitive information. See the ActiveSupport::ParameterFilter documentation for supported
 # notations and behaviors.
+
+# NOTE: 任意のカラムにフィルタをするときは、カラム名に fpl を含むようにする
 Rails.application.config.filter_parameters += [
-  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :fpl
 ]
