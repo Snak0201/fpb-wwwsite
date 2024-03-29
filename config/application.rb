79bdce577c7cf17f6d5b8917d2e744fd9cdad311
Snak <72hoshino.snak@gmail.com>
2024-03-29 12:46:16 +0900

    psychのエラーに対応する

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,9 @@ module App
 
     config.action_view.sanitized_allowed_tags = Loofah::HTML5::SafeList::ALLOWED_ELEMENTS
     config.action_view.sanitized_allowed_attributes = Loofah::HTML5::SafeList::ALLOWED_ATTRIBUTES
+
+    config.active_record.yaml_column_permitted_classes = [
+      Time
+    ]
   end
 end
