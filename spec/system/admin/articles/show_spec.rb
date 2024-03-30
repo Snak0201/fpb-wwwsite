c0bd6a066b7e1f773f72f84b68c58a7d88b2b2fc
Snak <72hoshino.snak@gmail.com>
2024-03-30 11:20:16 +0900

    管理画面上の記事復元リンクについてのspecを追加

--- spec/system/admin/articles/show_spec.rb
+++ spec/system/admin/articles/show_spec.rb
@@ -9,17 +9,41 @@ RSpec.describe 'show article in admin page' do
     admin_log_in admin
   end
 
-  context 'when the article is not published' do
-    it 'has right message about published_at' do
-      visit admin_article_path(unpublished_article)
-      within('#published_at') { expect(page).to have_content '公開されていません' }
+  describe '#published_at' do
+    context 'when the article is not published' do
+      it 'has right message about published_at' do
+        visit admin_article_path(unpublished_article)
+        within('#published_at') { expect(page).to have_content '公開されていません' }
+      end
+    end
+
+    context 'when the article is published' do
+      it 'has published_at' do
+        visit admin_article_path(published_article)
+        within('#published_at') { expect(page).to have_content I18n.l(published_article.published_at) }
+      end
     end
   end
 
-  context 'when the article is published' do
-    it 'has published_at' do
-      visit admin_article_path(published_article)
-      within('#published_at') { expect(page).to have_content I18n.l(published_article.published_at) }
+  describe 'restore link' do
+    context 'when the article does not have versions' do
+      it 'do not exist' do
+        visit admin_article_path(published_article)
+
+        expect(page).to have_no_link '記事を復元する', href: restore_previous_version_admin_article_path(published_article)
+      end
+    end
+
+    context 'when the article has versions' do
+      before do
+        published_article.update(title: '更新後のタイトル', content: '更新されたコンテンツ')
+      end
+
+      it 'exists' do
+        visit admin_article_path(published_article)
+
+        expect(page).to have_link '記事を復元する', href: restore_previous_version_admin_article_path(published_article)
+      end
     end
   end
 end
