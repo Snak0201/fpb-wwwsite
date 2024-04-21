e754a0f659f9bbe0aece8c155d534c977a1dd9d0
Snak <snak.hoshinonaka@gmail.com>
2024-04-21 17:13:11 +0900

    hover解除

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, "git@github.com:Snak0201/fpb-wwwsite.git"
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-set :branch, "main"
+set :branch, "135-fix-ios-button"
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/fpb-wwwsite"
