3387be9193fc4375c201625b8e7e81a18ce47390
Snak <snak.hoshinonaka@gmail.com>
2024-05-05 15:50:06 +0900

    graphql errorを修正する

--- app/graphql/resolvers/bureau_resolver.rb
+++ app/graphql/resolvers/bureau_resolver.rb
@@ -7,7 +7,7 @@ module Resolvers
     argument :slug, String, required: true, description: 'slug string of bureau'
 
     def resolve(slug:)
-      Bureau.find_by(slug:)
+      Bureau.find_by!(slug:)
     end
   end
 end
