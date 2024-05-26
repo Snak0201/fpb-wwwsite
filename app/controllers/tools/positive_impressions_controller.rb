12b40f460358e5391aa0ecb5c647ea3f8dfe599e
Snak <snak.hoshinonaka@gmail.com>
2024-05-26 21:26:54 +0900

    valueとの疎通確認

--- app/controllers/tools/positive_impressions_controller.rb
+++ app/controllers/tools/positive_impressions_controller.rb
@@ -1,5 +1,6 @@
 class Tools::PositiveImpressionsController < ApplicationController
   def show
-    
+    model = Tools::PositiveImpression.new(positive_impression_value: 2)
+    @value = model.show_value
   end
 end
--- app/models/tools/positive_impression.rb
+++ app/models/tools/positive_impression.rb
@@ -1,3 +1,11 @@
 class Tools::PositiveImpression
   include ActiveModel::Model
+
+  attr_accessor :positive_impression_value
+
+  validates :positive_impression_value, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  def show_value
+    positive_impression_value
+  end
 end
