b359cf804e3e1cf6be2a1d1c47937d8f04fa60ac
Snak <snak.hoshinonaka@gmail.com>
2024-06-01 19:36:49 +0900

    specファイルの追加

--- spec/factories/tools/positive_impressions.rb
+++ spec/factories/tools/positive_impressions.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :tools_positive_impression, class: 'Tools::PositiveImpression' do
-    
+    positive_impression_value {"0"}
   end
 end
--- spec/models/tools/positive_impression_spec.rb
+++ spec/models/tools/positive_impression_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Tools::PositiveImpression, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    let(:positive_impression){ build(:tools_positive_impression)}
+  end
+
+  describe "#transitions" do
+    context "with positive value" do
+      
+    end
+  end
+
+  describe "#status_gains" do
+
+  end
 end
