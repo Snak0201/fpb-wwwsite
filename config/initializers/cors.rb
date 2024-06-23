4ff9807d7433d322d02c53bda094795c89479603
Snak <snak.hoshinonaka@gmail.com>
2024-06-23 14:19:40 +0900

    originを設定する

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'hoshinonaka.net'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :options]
   end
 end
