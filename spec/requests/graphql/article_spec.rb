f5b25f455b642589363b41eebb08881853a6c826
Snak <snak.hoshinonaka@gmail.com>
2024-07-14 12:40:49 +0900

    firstの件数で正しく取得できることを検証する

--- spec/requests/graphql/article_spec.rb
+++ spec/requests/graphql/article_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Article' do
   describe 'POST /graphql' do
     subject(:request) { post '/graphql', params: { query: } }
+    let(:response_data) { JSON.parse(response.body, symbolize_names: true).dig(:data, :articles) }
+    let(:response_nodes) { response_data[:nodes] }
 
     before do
       create_list(:article, 7, :published)
@@ -10,8 +12,6 @@ RSpec.describe 'Article' do
     end
 
     context 'when requests articles query' do
-      let(:response_data) { JSON.parse(response.body, symbolize_names: true).dig(:data, :articles) }
-      let(:response_nodes) { response_data[:nodes] }
       let(:query) do
         <<~QUERY
           {
@@ -36,6 +36,35 @@ RSpec.describe 'Article' do
       it 'has published articles and sorted new published at' do
         request
         expect(response_nodes.length).to eq 7
+        expect(response_data).to eq expected_data
+      end
+    end
+
+    context 'when requests articles query small first' do
+      let(:query) do
+        <<~QUERY
+          {
+            articles(first: 3){
+              nodes{
+                id
+                title
+                content
+                number
+              }
+            }
+          }
+        QUERY
+      end
+
+      let(:expected_data) do
+        { nodes: Article.where.not(published_at: nil).order(published_at: :desc).limit(3).map do |article|
+          { id: article.id.to_s, title: article.title, content: article.content, number: article.number }
+        end }
+      end
+
+      it 'has specified number of articles in first' do
+        request
+        expect(response_nodes.length).to eq 3
         expect(response_data).to eq expected_data
       end
     end
