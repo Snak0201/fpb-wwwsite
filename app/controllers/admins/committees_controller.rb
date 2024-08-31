debfe061892aae68fe72cdb2fd6ab09821bb0e80
Snak <72hoshino.snak@gmail.com>
2024-08-31 20:49:46 +0900

    委員会更新機能を局からコピーする

--- app/controllers/admins/committees_controller.rb
+++ app/controllers/admins/committees_controller.rb
@@ -8,6 +8,21 @@ module Admins
       @committee = Committee.find_by!(slug: params[:slug])
     end
 
+    def update
+      @committee = Committee.find_by!(slug: params[:slug])
+
+      if params[:commit] == 'キャンセル'
+        @committee = Committee.new(committee_params)
+        render :edit, status: :see_other
+      elsif params[:commit] == '更新'
+        if @committee.update(committee_params)
+          redirect_to admin_committees_path, notice: '委員会を更新しました。'
+        else
+          render :edit, status: :unprocessable_entity
+        end
+      end
+    end
+
     def preview
       @committee = Committee.new(committee_params)
       # NOTE: slugの変更があった時、元のslugに対してリクエストしないと404になるため
