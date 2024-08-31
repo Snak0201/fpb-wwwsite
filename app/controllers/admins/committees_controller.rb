ee328ba336b7b3ac09fe7be5d24d7e51bd92542d
Snak <72hoshino.snak@gmail.com>
2024-08-31 20:41:09 +0900

    プレビュー画面を作成する

--- app/controllers/admins/committees_controller.rb
+++ app/controllers/admins/committees_controller.rb
@@ -9,7 +9,16 @@ module Admins
     end
 
     def preview
+      @committee = Committee.new(committee_params)
+      # NOTE: slugの変更があった時、元のslugに対してリクエストしないと404になるため
+      @update_target_committee = Committee.find_by!(slug: params[:slug])
+      render :preview, status: :see_other
+    end
 
+    private
+
+    def committee_params
+      params.require(:committee).permit(:name, :slug, :description, :content)
     end
   end
 end
