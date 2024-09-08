37c10f0c20a42acfcb5fed822184ee90426d39b6
Snak <snak.hoshinonaka@gmail.com>
2024-09-08 09:05:15 +0900

    委員会があるときに委員会を表示する

--- app/controllers/bureaus_controller.rb
+++ app/controllers/bureaus_controller.rb
@@ -2,5 +2,7 @@ class BureausController < ApplicationController
   def show
     @bureau = Bureau.find_by!(slug: params[:slug])
     @articles = @bureau.articles.published.page(params[:page]).per(10)
+    @committees = @bureau.committees.sorted
+    puts @committees.present?
   end
 end
