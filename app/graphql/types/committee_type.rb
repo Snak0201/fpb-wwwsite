fbf1a46941d57e9b661795cbaf1a97bb6105d068
Snak <snak.hoshinonaka@gmail.com>
2024-09-28 17:01:40 +0900

    委員会のtypeを追加する

---
+++ app/graphql/types/committee_type.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Types
+  class CommitteeType < Types::BaseObject
+    description 'article tags'
+
+    field :bureau_id, Integer, description: 'jurisdiction bureau id'
+    field :content, String, description: 'markdown content'
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false, description: 'created_at'
+    field :description, String, null: false, description: 'short description'
+    field :id, ID, null: false, description: 'id'
+    field :name, String, null: false, description: 'name'
+    field :slug, String, null: false, description: 'search keyword'
+    field :special, Boolean, null: false, description: 'is special committee'
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false, description: 'updated_at'
+  end
+end
