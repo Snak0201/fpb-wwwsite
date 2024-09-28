acb65a22f9aaaeea4fa423ad678287cf7e5e39e5
Snak <snak.hoshinonaka@gmail.com>
2024-09-28 17:13:59 +0900

    委員会一覧のresolverを追加する

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,5 +7,6 @@ module Types
     field :articles, resolver: Resolvers::ArticlesResolver, description: 'all published articles'
     field :bureau, resolver: Resolvers::BureauResolver, description: 'single bureau with slug'
     field :bureaus, resolver: Resolvers::BureausResolver, description: 'all bureaus'
+    field :committees, resolver: Resolvers::CommitteesResolver, description: 'all committees'
   end
 end
