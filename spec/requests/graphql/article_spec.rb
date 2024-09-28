57d8c23335691e728839c2d730a9ad79307a9872
Snak <snak.hoshinonaka@gmail.com>
2024-09-28 19:12:55 +0900

    記事のクエリから局と委員会を取り出せることをテストする

--- spec/requests/graphql/article_spec.rb
+++ spec/requests/graphql/article_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Article' do
-  describe 'POST /graphql' do
-    subject(:request) { post '/graphql', params: { query: } }
+  subject(:response_nodes) { response_data[:nodes] }
 
-    let(:response_data) { JSON.parse(response.body, symbolize_names: true).dig(:data, :articles) }
-    let(:response_nodes) { response_data[:nodes] }
+  let(:request) { post '/graphql', params: { query: } }
 
-    before do
-      create_list(:article, 7, :published)
-      create_list(:article, 3)
-    end
+  let(:response_data) { JSON.parse(response.body, symbolize_names: true).dig(:data, :articles) }
 
+  describe 'articles query' do
     context 'when requests articles query' do
+      before do
+        create_list(:article, 7, :published)
+        create_list(:article, 3)
+      end
+
       let(:query) do
         <<~QUERY
           {
@@ -42,6 +43,11 @@ RSpec.describe 'Article' do
     end
 
     context 'when requests articles query small first' do
+      before do
+        create_list(:article, 7, :published)
+        create_list(:article, 3)
+      end
+
       let(:query) do
         <<~QUERY
           {
@@ -66,6 +72,41 @@ RSpec.describe 'Article' do
       it 'has specified number of articles in first' do
         request
         expect(response_nodes.length).to eq 3
+        expect(response_data).to eq expected_data
+      end
+    end
+
+    context 'when article has jurisdiction bureaus and committees' do
+      let!(:article) { create(:article, :with_bureau, :with_committee, :published) }
+
+      let(:query) do
+        <<~QUERY
+          {
+            articles(first: 5){
+              nodes{
+                bureaus{
+                  name
+                }
+                committees{
+                  name
+                }
+              }
+            }
+          }
+        QUERY
+      end
+
+      let(:expected_data) do
+        {
+          nodes: [{
+            bureaus: [{ name: article.bureaus.first.name }],
+            committees: [{ name: article.committees.first.name }]
+          }]
+        }
+      end
+
+      it 'returns expected data' do
+        request
         expect(response_data).to eq expected_data
       end
     end
