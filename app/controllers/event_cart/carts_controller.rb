ed4f2e9352a34e5cf65efdcfa3f82a1df7d7077e
Snak <72hoshino.snak@gmail.com>
2025-01-02 02:35:09 +0000

    カート情報を更新する仕組みの作成

--- app/controllers/event_cart/carts_controller.rb
+++ app/controllers/event_cart/carts_controller.rb
@@ -15,21 +15,30 @@ module EventCart
     end
 
     def create
-      @cart = EventCart::Cart.new(create_cart_params)
+      @cart = EventCart::Cart.new(cart_params)
       if @cart.valid?
-        service = EventCart::CreateCart.run(create_cart_params)
+        service = EventCart::CreateCart.run(cart_params)
 
         if service.result
           redirect_to event_cart_cart_path(service.result), notice: '登録しました。URLを保存しておいてください。'
         else
-          redirect_to event_cart_root_path, alert: '登録に失敗しました。'
+          redirect_to event_cart_root_path, alert: '登録に失敗しました。再度やり直してください。'
         end
       else
         render :index, status: :unprocessable_entity
       end
     end
 
-    def update; end
+    def update
+      @target_cart = EventCart::Cart.enabled.find_by!(unique_code: params[:unique_code])
+
+      if @target_cart.update(cart_params)
+        redirect_to event_cart_cart_path(@target_cart.unique_code), notice: 'カート情報を保存しました。'
+      else
+        @cart = EventCart::Cart.new(cart_params)
+        render :edit, status: :unprocessable_entity
+      end
+    end
 
     def destroy
       @cart = EventCart::Cart.enabled.find_by!(unique_code: params[:unique_code])
@@ -43,7 +52,7 @@ module EventCart
 
     private
 
-    def create_cart_params
+    def cart_params
       params.require(:event_cart_cart).permit(:name, :held_at, :place, :atlas, :memo)
     end
 
