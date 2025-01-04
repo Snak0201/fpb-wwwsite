92d6ce4c3637c458f387d70f0de326b7b790cd90
Snak <72hoshino.snak@gmail.com>
2025-01-04 14:36:54 +0000

    ステータスを日本語で表示できるようにする

--- app/decorators/event_carts/mark_decorator.rb
+++ app/decorators/event_carts/mark_decorator.rb
@@ -2,6 +2,10 @@ module EventCarts
   class MarkDecorator < Draper::Decorator
     delegate_all
 
+    def translated_status
+      object.class.human_attribute_name("status.#{object.status}")
+    end
+
     def place
       "#{object.place_1st} #{object.place_2nd} #{object.place_3rd}"
     end
